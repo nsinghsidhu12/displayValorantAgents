9150fa716e229c2f41d512c1a6f96e32d034905a
nsinghsidhu12 <nsinghsidhu12@gmail.com>
2021-12-19 00:27:45 -0800

    added style.css to organize and display the agent info better

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# displayValorantAgents" 
+# displayValorantAgents
--- displayui.js
+++ displayui.js
@@ -1,9 +1,16 @@
 const agentName = document.querySelector("#agentName");
 const agentImage = document.querySelector("#agentImage");
+const agentRole = document.querySelector("#agentRole");
+const agentRoleDesc = document.querySelector("#agentRoleDesc");
+const agentDesc = document.querySelector("#agentDesc");
+
 
 class DisplayUI {
   AgentData(agent) {
     agentName.innerHTML = `${agent.displayName}`;
-    agentImage.src = `${agent.killfeedPortrait}`;
+    agentImage.src = `${agent.bustPortrait}`;
+    agentRole.innerHTML = `${agent.role.displayName}`;
+    agentRoleDesc.innerHTML = `${agent.role.description}`;
+    agentDesc.innerHTML = `${agent.description}`;
   }
 }
