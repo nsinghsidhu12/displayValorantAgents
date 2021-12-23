3f7f4c412a9604949c3ead7fb506c70e02c3831d
nsinghsidhu12 <nsinghsidhu12@gmail.com>
2021-12-23 02:03:35 -0800

    Displays error msg if input does not match an agent name

--- uuid.js
+++ uuid.js
@@ -1,5 +1,7 @@
 const ft = new Fetch();
-let err = false;
+
+const agentInput = document.querySelector("#agentInput");
+const agentSubmit = document.querySelector("#agentSubmit");
 const errorMessage = document.querySelector("#errorMessage");
 
 let defaultUUID = "41fb69c1-4189-7b37-f117-bcaf1e96f1bf";
@@ -8,31 +10,43 @@ if (localStorage.getItem("agent") != null) {
   defaultUUID = localStorage.getItem("agent");
 }
 
-const agentInput = document.querySelector("#agentInput");
-const agentSubmit = document.querySelector("#agentSubmit");
-
 class UUID {
-  async getAgentUUID() {
-    ft.getAgent(defaultUUID);
-    const agents = await ft.getAllAgents();
-    this.inputSubmit(this.filterAgents(agents));
+  async GetAgentUUID() {
+    ft.GetAgent(defaultUUID);
+    const agents = await ft.GetAllAgents();
+    this.InputClick();
+    this.InputSubmit(this.FilterAgents(agents));
   }
 
-  inputSubmit(newAgents) {
+  InputSubmit(newAgents) {
+    let err = true;
     agentSubmit.addEventListener("click", () => {
       newAgents.forEach(newAgent => {
         if (agentInput.value.toUpperCase() == newAgent.displayName.toUpperCase()) {
-          ft.getAgent(newAgent.uuid);
+          ft.GetAgent(newAgent.uuid);
           localStorage.setItem("agent", newAgent.uuid);
+          err = false;
         }
       })
+      if (err) {
+        errorMessage.textContent = `${agentInput.value} is not a valid agent`;
+      } else {
+        errorMessage.textContent = "";
+        err = true;
+      }
     })
   }
 
-  filterAgents(agents) {
+  InputClick() {
+    agentInput.addEventListener("click", () => {
+      errorMessage.textContent = "";
+    })
+  }
+
+  FilterAgents(agents) {
     return agents.filter(agent => agent.uuid != "ded3520f-4264-bfed-162d-b080e2abccf9");
   }
 }
 
 const uuid = new UUID();
-uuid.getAgentUUID();
+uuid.GetAgentUUID();
